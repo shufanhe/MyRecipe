d0e31fd8f8df9361379882a113c9a97291a834af
khanhta2001 <ktran2@iwu.edu>
2022-11-15 13:18:40 -0600

    added unitTests for authentication

--- unittests.py
+++ unittests.py
@@ -20,6 +20,40 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(app.app.config['DATABASE'])
 
+    def register(self, username, email, password):
+        return self.app.post('/register',
+                             data=dict(username=username, Email=email, password=password, RetypePassword=password),
+                             follow_redirects=True)
+
+    def login(self,username,password):
+        return self.app.post('/login',data=dict(username=username,password=password), follow_redirects=True)
+
+    def test_homePage(self):
+        rv = self.app.get('/')
+        assert b'<title>MyRecipe</title>' in rv.data
+        assert b'Recipe of the Day!' in rv.data
+        assert b'Search For Recipes!' in rv.data
+        assert b'Login' in rv.data
+        assert b'Register' in rv.data
+
+    def test_authentication(self):
+        rv = self.register('khanhta2001', 'khanhta2001@gmail.com', 'testing1234!')
+        assert b'Login' in rv.data
+        assert b'Username/Email' in rv.data
+        assert b'Password' in rv.data
+        rv = self.login('khanhta2001','testing1234!')
+        assert b'<title>MyRecipe</title>' in rv.data
+        assert b'Recipe of the Day!' in rv.data
+        assert b'Search For Recipes!' in rv.data
+        assert b'Logout' in rv.data
+        assert b'User' in rv.data
+        rv = self.app.get('/logout',follow_redirects=True)
+        assert b'<title>MyRecipe</title>' in rv.data
+        assert b'Recipe of the Day!' in rv.data
+        assert b'Search For Recipes!' in rv.data
+        assert b'Login' in rv.data
+        assert b'Register' in rv.data
+
 
 if __name__ == '__main__':
     unittest.main()
